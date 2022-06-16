0e90e12e858b62c4d10e95871a38700bbf43a3af
Lubomir Rajter <lubomir.rajter@gmail.com>
2022-06-16 08:00:11 +0200

    check answer function added into main.py

--- FaVoriTe/main.py
+++ FaVoriTe/main.py
@@ -8,7 +8,6 @@ words = pd.read_csv('/home/lubo/code/wRajter/FaVoriTe/FaVoriTe/data/german_words
 dict_words = words.to_dict(orient="records")
 
 
-
 def random_word():
     '''returns a random word and its translated version'''
     return (random.choice(dict_words))
@@ -25,27 +24,28 @@ def clean_lower(word):
     '''returns word witout diactitics and lowercase'''
     return unidecode(word).lower() # unidecode removes diacritics
 
-def checking_answer(user_input, current_word):
-        user_input_prep = clean_lower(user_input)
-        answer_split = slovak_word(current_word).split('/')   # split into possible answers if more than one
-        answer_adjusted = [unidecode(word.lower())
-                           for word
-                           in answer_split] # removing diacritics and making lowercase letters
-        return answer_adjusted
-        # if user_input in answer_adjusted:
-        #     self.question.config(text=f"Správne!\n{self.current_word['German']} znamená {', '.join(answer_adjusted)}")
-        #     self.check_button.config(text="Dalšie slovo", command=self.next_word)
-        #     self.translation_entry.bind('<Shift_R>', self.next_word)
-        # else:
-        #     self.question.config(text=f"'{self.current_word['German']}' znamená {', '.join(answer_adjusted)}")
-        #     self.check_button.config(text="Dalšie slovo", command=self.next_word)
-        #     self.translation_entry.bind('<Shift_R>', self.next_word)
+def answer_split(current_word):
+    '''returns list of possible answers if multiple answers in current word'''
+    answer_split = slovak_word(current_word).split('/')   # split into possible answers if more than one
+    list_answers = [clean_lower(word)
+                        for word
+                        in answer_split] # removing diacritics and making lowercase letters
+    return list_answers
 
+def check_answer(current_word, user_input, list_answers):
+    '''checkes if the user answer was correct or not.
+    Please specify: current_word, user_input, list_answers'''
+    user_input_prep = clean_lower(user_input)
+    if user_input_prep in list_answers:
+        return f"Správne!\n{german_word(current_word)} znamená {', '.join(list_answers)}"
+    else:
+        return f"{german_word(current_word)} znamená {', '.join(list_answers)}"
 
 
 if __name__ == '__main__':
     current_word = random_word()
-    print(slovak_word(current_word))
-    user_input = input(f'please translate {german_word(current_word)} into slovak')
-    answer_adjusted = checking_answer(user_input, current_word)
-    print(answer_adjusted)
+    print(f'slovak version: {slovak_word(current_word)}')
+    user_input = input(f'please translate {german_word(current_word)} into slovak:   ')
+    list_answers = answer_split(current_word)
+    output = check_answer(current_word, user_input, list_answers)
+    print(output)
