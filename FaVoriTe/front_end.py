4d6934ae9a6efb1d7101071103be12118572353e
Lubomir Rajter <lubomir.rajter@gmail.com>
2022-06-17 17:01:33 +0200

    front end: generate answer funtionality added

--- FaVoriTe/front_end.py
+++ FaVoriTe/front_end.py
@@ -1,3 +1,4 @@
+from nbformat import write
 import streamlit as st
 import main_functions as mf
 
@@ -8,13 +9,14 @@ dataset = st.container()
 train = st.container()
 
 
+
 with header:
     st.title('Welcome to FaVoriTe, your own Fast Vocabulary Trainer 🥋')
 
 
 with options:
     st.sidebar.header('Options')
-    lang_trainer = st.sidebar.selectbox('Select the language in which you want to have words for translation displayed.', ['English', 'German', 'Slovak'])
+    lang_trainer = st.sidebar.selectbox('Select the language for a word that you will translate.', ['English', 'German', 'Slovak'])
     lang_user = st.sidebar.selectbox('Select the language in which you want to answer', ['English', 'German', 'Slovak'])
     size = st.sidebar.slider(label='Number of words you want to train with', min_value=10, max_value=100, value=10, step=10)
     data = mf.get_data(size=size)
@@ -22,5 +24,23 @@ with options:
 
 
 
+if "input_text" not in st.session_state:
+    st.session_state['input_text'] = ''
+
+if 'current_word' not in st.session_state:
+    st.session_state['current_word'] = mf.random_word(data)
+
+
 with train:
     st.header('Your Workout 🏋️🧠')
+    next_word = st.button('Gimme a word')
+    if next_word:
+        st.session_state['current_word'] = mf.random_word(data)
+
+    user_input = st.text_input(f'Please translate {mf.german_word(st.session_state["current_word"])} into Slovak', key="input_text")
+    if st.session_state['input_text'] == '':
+        st.write('')
+    else:
+        user_input = st.session_state.input_text
+        list_answers = mf.answer_split(st.session_state["current_word"])
+        st.write(mf.check_answer(st.session_state["current_word"], user_input, list_answers))
